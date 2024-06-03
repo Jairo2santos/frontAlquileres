9bc27f64d4deb7916d4f8ab63c6fcffe303d963f
Jairo2santos <107949575+Jairo2santos@users.noreply.github.com>
2024-06-03 20:28:55 -0300

    last changes including vuetify
    asd

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+      <v-main>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-card>
+                <v-card-title>Bienvenido a Vuetify con Nuxt 3</v-card-title>
+                <v-card-text>
+                  Este es un ejemplo de una tarjeta usando Vuetify en un proyecto Nuxt 3.
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup lang="ts">
+  </script>
+  
+  <style scoped>
+  </style>
+  
